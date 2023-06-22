b2da0cbb05635467981f03c7513be6a151193492
heather-cunningham <cunningham.heatherirene@gmail.com>
2023-06-22 14:36:11 -0400

    beginApp: More set up for iOS camera use and android sdk

--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,9 @@ class TableTopRpgDiceCalculator extends StatelessWidget {
       title: 'TableTop Rpg Dice Calculator',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const HomePage(title: 'TableTop Rpg Dice Calculator Home Page'),
+      home: const HomePage(title: 'TableTop Rpg Dice Calculator'),
     );
   }
 }
